6880ebedbedf67d99efd82cda9b83c4f0b60221d
Tertius Kabugi <kingkabugi@gmail.com>
2023-03-31 17:28:54 +0300

    test added!!

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ df.drop('_c39', axis=1, inplace=True)
 df['policy_bind_year'] = df['policy_bind_date'].str.extract('(\d{4})\-').astype('int32')
 
 # extract the month out
-# all accidents are from 2015 jan and feb, so year isnt very important
+# all accidents are from 2015 jan and feb, so year is not very important
 df['incident_month'] = df['incident_date'].str.extract('\d{4}\-(\d{2})').astype('int32')
 
 # replace "?" with undocumented
@@ -174,58 +174,58 @@ print(list(cont_var))
 print('nominal variables:{}'.format(len(nom_var)))
 print(list(nom_var))
 
-# # count each level of the Dv
-# df.fraud_reported.value_counts()
-#
-# # EXPLORATORY
-# # count each level of the Dv
-# df.fraud_reported.value_counts()
-#
-# # proportion of each level of DV
-# df.fraud_reported.value_counts(normalize=True)
-#
-# # dist of dv
-# # plt.style.use('dark_background') for ppt
-# sns.set()
-# sns.countplot(x="fraud_reported", data=df, palette=['r', 'b']);
-#
-#
-# # variable correlation
-# # Color negative numbers red
-# def color(val):
-#     color = 'green' if val == 1 else 'red' if val < -0.3 else 'blue' if val > 0.3 else 'black'  # write like lambda
-#     return 'color: %s' % color
-#
-#
-# corr = df[cont_var].corr()
-# corr.style.applymap(color)
-#
-# sns.set_style('white')
-#
-# # heatmap from those with at least 0.3 magnitude in corr, includeing the DV
-# corr_list = ['age', 'months_as_customer', 'total_claim_amount',
-#              'injury_claim', 'property_claim', 'vehicle_claim',
-#              'incident_severity', 'fraud_reported']
-#
-# corr_df = df[corr_list]
-# corr = round(corr_df.corr(), 2)
-#
-# # Set the default matplotlib figure size to 7x7:
-# fix, ax = plt.subplots(figsize=(10, 10))
-#
-# # Generate a mask for the upper triangle (taken from seaborn example gallery)
-# mask = np.zeros_like(corr, dtype=np.bool)
-# mask[np.triu_indices_from(mask)] = True  # triu triangle upper
-#
-# # Plot the heatmap with seaborn.
-# # Assign the matplotlib axis the function returns. This will let us resize the labels.
-# ax = sns.heatmap(corr, mask=mask, ax=ax, annot=True, cmap='OrRd')
-#
-# # Resize the labels.
-# ax.set_xticklabels(ax.xaxis.get_ticklabels(), fontsize=10, ha='right', rotation=45)
-# ax.set_yticklabels(ax.yaxis.get_ticklabels(), fontsize=10, va="center", rotation=0)
-#
-# # If you put plt.show() at the bottom, it prevents those useless printouts from matplotlib.
-# plt.show()
-#
-# # visualization
+# count each level of the Dv
+df.fraud_reported.value_counts()
+
+# EXPLORATORY
+# count each level of the Dv
+df.fraud_reported.value_counts()
+
+# proportion of each level of DV
+df.fraud_reported.value_counts(normalize=True)
+
+# dist of dv
+# plt.style.use('dark_background') for ppt
+sns.set()
+sns.countplot(x="fraud_reported", data=df, palette=['r', 'b']);
+
+
+# variable correlation
+# Color negative numbers red
+def color(val):
+    color = 'green' if val == 1 else 'red' if val < -0.3 else 'blue' if val > 0.3 else 'black'  # write like lambda
+    return 'color: %s' % color
+
+
+corr = df[cont_var].corr()
+corr.style.applymap(color)
+
+sns.set_style('white')
+
+# heatmap from those with at least 0.3 magnitude in corr, includeing the DV
+corr_list = ['age', 'months_as_customer', 'total_claim_amount',
+             'injury_claim', 'property_claim', 'vehicle_claim',
+             'incident_severity', 'fraud_reported']
+
+corr_df = df[corr_list]
+corr = round(corr_df.corr(), 2)
+
+# Set the default matplotlib figure size to 7x7:
+fix, ax = plt.subplots(figsize=(10, 10))
+
+# Generate a mask for the upper triangle (taken from seaborn example gallery)
+mask = np.zeros_like(corr, dtype=bool)
+mask[np.triu_indices_from(mask)] = True  # triu triangle upper
+
+# Plot the heatmap with seaborn.
+# Assign the matplotlib axis the function returns. This will let us resize the labels.
+ax = sns.heatmap(corr, mask=mask, ax=ax, annot=True, cmap='OrRd')
+
+# Resize the labels.
+ax.set_xticklabels(ax.xaxis.get_ticklabels(), fontsize=10, ha='right', rotation=45)
+ax.set_yticklabels(ax.yaxis.get_ticklabels(), fontsize=10, va="center", rotation=0)
+
+# If you put plt.show() at the bottom, it prevents those useless printouts from matplotlib.
+plt.show()
+
+# visualization
